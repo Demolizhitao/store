5fa2145a81f518f0a27a85ee9d3070a628ad2cb4
my-git <757022544@qq.com>
2019-02-12 10:58:33 +0800

    完成功能：文件上传名字加密

--- src/main/java/cn/tedu/spring/FileController.java
+++ src/main/java/cn/tedu/spring/FileController.java
@@ -2,6 +2,7 @@ package cn.tedu.spring;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +19,25 @@ public class FileController {
     @RequestMapping(value="upload.do",method=RequestMethod.POST)
     public String handleUpload(
             @RequestParam("file") MultipartFile file) throws IllegalStateException, IOException{
+        //上传文件夹
+        File parent = new File("d:/");
+        
+        //上传的文件名
+       String originalFilename = 
+               file.getOriginalFilename();//获取原来文件名
+       //获取原扩展名
+       String suffix = "";
+       Integer index = originalFilename.lastIndexOf(".");
+       if(index != -1){
+         suffix = originalFilename.substring(index
+                   );
+       }
+               
+       
+        String child =UUID.randomUUID()+suffix;
+        
         //保存用户上传的文件
-        File dest = new File("d:/1.txt");
+        File dest = new File(parent,child);
         file.transferTo(dest);
         return "OK";
     }
